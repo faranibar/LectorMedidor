39a238f6bad2cdff27038225c365fd1c9580e3b9
franklin <fgaranibar@gmail.com>
2017-08-10 20:49:13 -0400

    New changelist

--- LectorMedidor/app/src/main/java/otb/delicias/lectormedidor/LoginActivity.java
+++ LectorMedidor/app/src/main/java/otb/delicias/lectormedidor/LoginActivity.java
@@ -137,6 +137,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         }
     }
 
+
+
+
+
+
     /**
      * Attempts to sign in or register the account specified by the login form.
      * If there are form errors (invalid email, missing fields, etc.), the
